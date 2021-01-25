668bae0900613ca4f8c1947ce5fca67f9d99f3f4
min <lvxinjieao@163.com>
2021-01-25 18:40:35 +0800

    sql

---
+++ SQL/2020-06-01-union-packer.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 聚合
+Source Server Version : 50732
+Source Host           : 119.23.56.194:3306
+Source Database       : 2020-06-01-union-packer
+
+Target Server Type    : MYSQL
+Target Server Version : 50732
+File Encoding         : 65001
+
+Date: 2021-01-25 18:32:26
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
